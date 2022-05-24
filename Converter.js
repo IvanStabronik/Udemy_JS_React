294b44b240d0d187dda8fa384ac71e7b60288800
Ivan Stabronik <engel.stabronik@gmail.com>
2022-05-24 12:45:13 +0300

    first commit

---
+++ Converter.js
@@ -0,0 +1,59 @@
+const usdCurr = 28;
+const discount = 0.9;
+
+function convert(amount, curr) {
+    return curr * amount;
+}
+
+function promotion(result) {
+    console.log(result * discount);
+}
+
+const res = convert(500, usdCurr);
+promotion(res);
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) return
+    }
+    console.log('done');
+}
+
+test();
+
+function doNothing() {};
+console.log(doNothing() === undefined);
+
+
+// Место для первой задачи
+function sayHello(name) {
+    return `Привет, ${name}`;
+}
+
+// Место для второй задачи
+function returnNeighboringNumbers(num) {
+    return  [num - 1, num, num +1];
+}
+
+// Место для третьей задачи
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
