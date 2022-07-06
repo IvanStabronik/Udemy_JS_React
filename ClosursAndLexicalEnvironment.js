a88e366613750514187240388c485e706e4d6bff
Ivan Stabronik <engel.stabronik@gmail.com>
2022-07-06 11:07:02 +0300

    fourth commit

---
+++ ClosursAndLexicalEnvironment.js
@@ -0,0 +1,82 @@
+"use strict";
+
+// let number = 5; debugger
+//
+// function logNumber() {
+//     let number = 4; debugger
+//     console.log(number);
+// }
+//
+// number = 6;
+//
+// logNumber();debugger
+//
+// number = 8;
+//
+// logNumber();debugger
+
+// function createCounter() {
+//     let counter = 0;
+//
+//     const myFunction = function () {
+//         counter = counter + 1;debugger
+//         return counter; debugger
+//     }
+//
+//     return myFunction();
+// }
+//
+// const increment = createCounter();
+// const c1 = increment();debugger
+// const c2 = increment();debugger
+// const c3 = increment();debugger
+//
+// console.log(c1, c2, c3);
+//
+// {
+//     let msg = 'Hello';
+// }
+//
+// console.log(msg);
+//
+//
+//
+//
+// for (let i = 0; i < 9; i++) {
+//     for (let j = 0; j < 9; j++) {
+//         let num = 3;
+//     }
+//
+//     console.log(num);
+// }
+
+// const result = getSum(5, 6);
+// const getSum = function(a, b) {
+//     return a + b;
+// }
+//
+// console.log(typeof(NaN));
+
+//
+// console.log(0 || 1);
+// console.log(0 && 1);
+// console.log('a' > 'b');
+// console.log(0 || NaN || false || null);
+
+// for (let i = 1; i < 5; i++) {
+//     console.log(i + 1)
+// }
+
+// function foo(a,b) {
+//     const [first] = a;
+//     const {eng} = b;
+//
+//     return `${first} ${eng}`;
+// }
+//
+// const result = foo(['Hello', 'Привет'], {ru: 'Мир', eng: 'World'})
+
+let time = '';
+5 > 3 || 2 ? time = 'Day' : time = 'Night'
+
+console.log(time);
